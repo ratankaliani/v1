504f819d0a543b4ad902711b146f48e87c9f77d0
Ratan Kaliani <ratankaliani@berkeley.edu>
2020-05-24 20:48:12 -0400

    putting in particles

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
+import Particles from 'react-particles-js';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+      <div className="particle-canvas">
+        <Particles
+          params={{
+            "particles": {
+              "number": {
+                "value": 50
+              },
+              "size": {
+                "value": 3
+              }
+            },
+            "interactivity": {
+              "events": {
+                "onhover": {
+                  "enable": true,
+                  "mode": "repulse"
+                }
+              }
+            }
+          }}
+        />
+      </div>
+    );
+  }
 
 export default App;
