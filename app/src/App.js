7a8d8c59302f3413b63bf342cf151c16bf630422
Ratan Kaliani <ratankaliani@berkeley.edu>
2020-05-27 02:22:04 -0400

    Added social icons, navigation buttons

--- app/src/App.js
+++ app/src/App.js
@@ -2,52 +2,126 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import Particles from 'react-particles-js';
 import './App.css';
-import {AwesomeButton} from 'react-awesome-button';
+import Button from 'react-bootstrap/Button'
+import { AwesomeButton } from "react-awesome-button";
+import AwesomeButtonStyles from "react-awesome-button/src/styles/styles.scss";
+import { SocialIcon } from 'react-social-icons';
+
+
+
+
 
 function App() {
     return (
       <div className="App">
-        <div id="intro">
+        <div className="intro">
           <div id="name"> 
             Ratan Kaliani
           </div>
-          <div id = "">
-            <AwesomeButton type="primary">
-            Button
-            </AwesomeButton>
+          
+        </div>
+        <div className="navigation">
+          <div className="button">
+              <AwesomeButton type="primary">
+                Primary</AwesomeButton>{' '}
           </div>
-          <div id = "">
-            <AwesomeButton type="primary">
-            Button
-            </AwesomeButton>
+          <div className="button">
+          <AwesomeButton type="primary"
+            >Primary</AwesomeButton>{' '}
           </div>
+          <div className="button">
+          <AwesomeButton type="primary"
+            >Primary</AwesomeButton>{' '}
+          </div>
+          <div className="button">
+          <AwesomeButton type="primary"
+            >Primary</AwesomeButton>{' '}
+          </div>
+        </div>
+        <div className="socialicons">
+          <div className="icon">
+            <SocialIcon
+              url = "mailto:ratan@berkeley.edu"
+              target = "_blank"
+            />
+          </div>
+          <div className="icon">
+            <SocialIcon
+              url = "https://twitter.com/ratankaliani"
+              target = "_blank"
+            />
+          </div>
+          <div className="icon">
+            <SocialIcon
+              url = "https://instagram.com/ratankaliani"
+              target = "_blank"
+            />
+          </div>
+          <div className="icon">
+            <SocialIcon
+              url = "https://github.com/ratankaliani"
+              target = "_blank"
+            />
+          </div>
+          <div className="icon">
+            <SocialIcon
+              url = "https://linkedin.com/in/ratankaliani"
+              target = "_blank"
+            />
+          </div>
+          <div className="icon">
+            <SocialIcon
+              url = "https://medium.com/ratankaliani"
+              target = "_blank"
+            />
+          </div>
+          
         </div>
         <div id="particle-canvas">
           
           <Particles
             params={{
               "particles": {
-                "color": {
-                  "value": "#ffffff"
-                },
-                "number": {
-                  "value": 100
-                },
-                "opacity": {
-                  "value": 0.4
-                },
-                "size": {
-                  "value": 2
-                }
+                  "number": {
+                      "value": 60,
+                      "density": {
+                          "enable": true,
+                          "value_area": 1500
+                      }
+                  },
+                  "line_linked": {
+                      "enable": true,
+                      "opacity": 0.5
+                  },
+                  "move": {
+                      "direction": "right",
+                      "speed": 0.1
+                  },
+                  "size": {
+                      "value": 1
+                  },
+                  "opacity": {
+                      "anim": {
+                          "enable": true,
+                          "speed": 1,
+                          "opacity_min": 0.5
+                      }
+                  }
               },
               "interactivity": {
-                "events": {
-                  "onhover": {
-                    "enable": true,
-                    "mode": "repulse"
+                  "events": {
+                      "onclick": {
+                          "enable": true,
+                          "mode": "push"
+                      }
+                  },
+                  "modes": {
+                      "push": {
+                          "particles_nb": 1
+                      }
                   }
-                }
-              }
+              },
+              "retina_detect": true
             }}
           />
         </div>
